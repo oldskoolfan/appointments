3323eba7e58cfefb4d91753a4a0c6fe6592208c0
Andrew Harris <harris.1305@gmail.com>
2016-11-06 12:30:57 -0500

    db update

--- include/mysql-connect.php
+++ include/mysql-connect.php
@@ -3,4 +3,4 @@ $config = parse_ini_file("{$_SERVER['DOCUMENT_ROOT']}/config.ini");
 $etcPath = $config['etc_directory'];
 include "$etcPath/db-connect.php";
 
-$con->select_db('appointmentdb');
+$con->select_db('andrewfharriscom_appointmentdb');
